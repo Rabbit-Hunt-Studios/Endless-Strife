0b6742d21ce33b5ea165f29b53f0f4c21e50ea3c
Pakorn Laohakanniyom <66674210+RyukungG@users.noreply.github.com>
2025-01-13 16:41:32 +0700

    Fix ground prefab (#9)
    * update gitignore to exclude assettesting meta files
    
    * fix highlights on ground prefabs
    
    * Add sword man prefab
    
    * add all cyan unit prefab
    
    * add mask in unit prefab for player side
    
    * add cell prefab
    
    ---------
    
    Co-authored-by: nifujilol <khunpunn2546@gmail.com>

---
+++ Assets/TBS Framework/Scripts/Units/ESUnit.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TbsFramework.Units;
+using TbsFramework.Cells;
+using EndlessStrife.Cells;
+
+public class ESUnit : Unit
+{
+    public string UnitName;
+    public Vector3 Offset;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        transform.localPosition += Offset;
+    }
+
+    public override void MarkAsDestroyed()
+    {
+    }
+
+    protected override int Defend(Unit other, int damage)
+    {
+        return damage - (Cell as ESSquare).DefenceBoost;
+    }
+
+    protected override AttackAction DealDamage(Unit unitToAttack)
+    {
+        var baseVal = base.DealDamage(unitToAttack);
+        var newDmg = TotalHitPoints == 0 ? 0 : (int)Mathf.Ceil(baseVal.Damage * ((float)HitPoints / TotalHitPoints));
+
+        return new AttackAction(newDmg, baseVal.ActionCost);
+    }
+
+    public override IEnumerator Move(Cell destinationCell, IList<Cell> path)
+    {
+        GetComponent<SpriteRenderer>().sortingOrder += 10;
+        transform.Find("Marker").GetComponent<SpriteRenderer>().sortingOrder += 10;
+        yield return base.Move(destinationCell, path);
+    }
+
+    protected override void OnMoveFinished()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder -= 10;
+        transform.Find("Marker").GetComponent<SpriteRenderer>().sortingOrder -= 10;
+        base.OnMoveFinished();
+    }
+
+    public override void SetColor(Color color)
+    {
+        var highlighter = transform.Find("Marker");
+        var spriteRenderer = highlighter.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = color;
+        }
+    }
+
+    public override void OnMouseDown()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            base.OnMouseDown();
+        }
+    }
+
+    public override void OnMouseEnter()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            base.OnMouseEnter();
+        }
+        Cell.MarkAsHighlighted();
+    }
+
+    public override void OnMouseExit()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            base.OnMouseExit();
+        }
+        Cell.UnMark();
+    }
+}
