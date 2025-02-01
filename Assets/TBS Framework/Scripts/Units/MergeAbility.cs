7f7e26784b2a2b1f04359165aa7c8bee254611c3
nifujilol <khunpunn2546@gmail.com>
2025-02-01 15:58:51 +0700

    check for mergable and merge UI done

---
+++ Assets/TBS Framework/Scripts/Units/MergeAbility.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TbsFramework.Units;
+using TbsFramework.Grid;
+using TbsFramework.Grid.GridStates;
+using TbsFramework.Units.Abilities;
+
+
+namespace TbsFramework.Units
+{
+    public class MergeAbility : Ability
+    {
+        public Unit unitToMerge { get; set; }
+        public HashSet<Unit> availableMerges;
+        public GameObject MergeButton;
+        public GameObject UnitPanel;
+        private List<GameObject> MergeButtons = new List<GameObject>();
+
+        public override IEnumerator Act(CellGrid cellGrid, bool isNetworkInvoked = false)
+        {
+            if (UnitReference.ActionPoints > 0 && availableMerges.Contains(unitToMerge))
+            {
+                // to be added
+            }
+            yield return base.Act(cellGrid, isNetworkInvoked);
+        }
+        public override void Display(CellGrid cellGrid)
+        {
+            if (UnitReference.ActionPoints > 0)
+            {
+                foreach (var unit in availableMerges)
+                {
+                    unit.MarkAsSelected();
+
+                    var unitButton = Instantiate(MergeButton, MergeButton.transform.parent);
+                    unitButton.GetComponent<Button>().interactable = true;
+                    unitButton.GetComponentInChildren<Button>().onClick.AddListener(() => ActWrapper(unit, cellGrid));
+
+                    unitButton.GetComponent<Button>().transform.Find("UnitImage").GetComponent<Image>().sprite = unit.GetComponent<SpriteRenderer>().sprite;
+                    unitButton.GetComponent<Button>().transform.Find("NameText").GetComponent<Text>().text = unit.GetComponent<ESUnit>().UnitName;
+
+                    unitButton.SetActive(true);
+                    MergeButtons.Add(unitButton);
+                }
+                UnitPanel.SetActive(true);
+            }
+        }
+
+        void ActWrapper(Unit unit, CellGrid cellGrid)
+        {
+            unitToMerge = unit;
+            StartCoroutine(Execute(cellGrid,
+                    _ => cellGrid.cellGridState = new CellGridStateBlockInput(cellGrid),
+                    _ => cellGrid.cellGridState = new CellGridStateWaitingForInput(cellGrid)));
+        }
+
+        public override void OnAbilitySelected(CellGrid cellGrid)
+        {
+            availableMerges = GetAvailableMerges(cellGrid);
+        }
+
+        public override void CleanUp(CellGrid cellGrid)
+        {
+            foreach (var unit in availableMerges)
+            {
+                unit.UnMark();
+            }
+            foreach (var button in MergeButtons)
+            {
+                Destroy(button);
+            }
+            UnitPanel.SetActive(false);
+        }
+
+        public override bool CanPerform(CellGrid cellGrid)
+        {
+            return UnitReference.ActionPoints > 0 && GetAvailableMerges(cellGrid).Count > 0;
+        }
+
+        public override IDictionary<string, string> Encapsulate()
+        {
+            var actionParams = new Dictionary<string, string>();
+
+            actionParams.Add("unit_x", unitToMerge.transform.localPosition.x.ToString());
+            actionParams.Add("unit_y", unitToMerge.transform.localPosition.y.ToString());
+
+            return actionParams;
+        }
+
+        public override IEnumerator Apply(CellGrid cellGrid, IDictionary<string, string> actionParams, bool isNetworkInvoked = false)
+        {
+            var targetUnit = cellGrid.Units.Find(u => u.transform.localPosition.Equals(new UnityEngine.Vector3(float.Parse(actionParams["unit_x"]), float.Parse(actionParams["unit_y"]), -0.01f)));
+            unitToMerge = targetUnit;
+            yield return StartCoroutine(RemoteExecute(cellGrid));
+        }
+
+        private HashSet<Unit> GetAvailableMerges(CellGrid cellGrid) 
+        {
+            var unitsFriendly = cellGrid.Units.FindAll(u => u.PlayerNumber == cellGrid.CurrentPlayer.PlayerNumber);
+            var result = new HashSet<Unit>();
+            var unit_x = System.Math.Round(UnitReference.transform.localPosition.x, 2);
+            var unit_y = System.Math.Round(UnitReference.transform.localPosition.y, 2);
+            
+            foreach (var unit in unitsFriendly)
+            {
+                if(!unit.Equals(UnitReference) && !unit.GetComponent<ESUnit>().isStructure)
+                {
+                    var otherUnit_x = System.Math.Round(unit.transform.localPosition.x, 2);
+                    var otherUnit_y = System.Math.Round(unit.transform.localPosition.y, 2);
+
+                    if (otherUnit_x.Equals(unit_x) &&
+                        (otherUnit_y.Equals(System.Math.Round(unit_y - 0.16f, 2)) || otherUnit_y.Equals(System.Math.Round(unit_y + 0.16f, 2))))
+                    {
+                        result.Add(unit);
+                    }
+                    else if (otherUnit_y.Equals(unit_y) &&
+                             (otherUnit_x.Equals(System.Math.Round(unit_x - 0.16f, 2)) || otherUnit_x.Equals(System.Math.Round(unit_x + 0.16f, 2))))
+                    {
+                        result.Add(unit);
+                    }
+                }
+            }
+            Debug.Log("found " + result.Count.ToString() + " adjacent");
+            return result;
+        }
+    }
+}
