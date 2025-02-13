c53859dec4dcd57e1ca972b61fd0c66009e79263
nifujilol <khunpunn2546@gmail.com>
2025-02-13 19:28:26 +0700

    fix no merge available button bug

--- Assets/TBS Framework/Scripts/Units/MergeButtonHover.cs
+++ Assets/TBS Framework/Scripts/Units/MergeButtonHover.cs
@@ -40,39 +40,42 @@ public class MergeButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     public void ShowMergePreview()
     {
-        var unit = unitToMerge.GetComponent<MergeStats>();
+        if (unitToMerge != null)
+        {
+            var unit = unitToMerge.GetComponent<MergeStats>();
 
-        var healthCard = Instantiate(StatCard, StatCard.transform.parent);
-        healthCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.HitPoints);
-        healthCard.transform.GetChild(1).GetComponent<Text>().text = unit.HitPoints.ToString();
-        healthCard.SetActive(true);
-        StatDisplays.Add(healthCard);
+            var healthCard = Instantiate(StatCard, StatCard.transform.parent);
+            healthCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.HitPoints);
+            healthCard.transform.GetChild(1).GetComponent<Text>().text = unit.HitPoints.ToString();
+            healthCard.SetActive(true);
+            StatDisplays.Add(healthCard);
 
-        var attackCard = Instantiate(StatCard, StatCard.transform.parent);
-        attackCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.Attack);
-        attackCard.transform.GetChild(1).GetComponent<Text>().text = unit.Attack.ToString();
-        attackCard.SetActive(true);
-        StatDisplays.Add(attackCard);
+            var attackCard = Instantiate(StatCard, StatCard.transform.parent);
+            attackCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.Attack);
+            attackCard.transform.GetChild(1).GetComponent<Text>().text = unit.Attack.ToString();
+            attackCard.SetActive(true);
+            StatDisplays.Add(attackCard);
 
-        var defenceCard = Instantiate(StatCard, StatCard.transform.parent);
-        defenceCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.Defence);
-        defenceCard.transform.GetChild(1).GetComponent<Text>().text = unit.Defence.ToString();
-        defenceCard.SetActive(true);
-        StatDisplays.Add(defenceCard);
+            var defenceCard = Instantiate(StatCard, StatCard.transform.parent);
+            defenceCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.Defence);
+            defenceCard.transform.GetChild(1).GetComponent<Text>().text = unit.Defence.ToString();
+            defenceCard.SetActive(true);
+            StatDisplays.Add(defenceCard);
 
-        var rangeCard = Instantiate(StatCard, StatCard.transform.parent);
-        rangeCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.AttackRange);
-        rangeCard.transform.GetChild(1).GetComponent<Text>().text = unit.AttackRange.ToString();
-        rangeCard.SetActive(true);
-        StatDisplays.Add(rangeCard);
+            var rangeCard = Instantiate(StatCard, StatCard.transform.parent);
+            rangeCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.AttackRange);
+            rangeCard.transform.GetChild(1).GetComponent<Text>().text = unit.AttackRange.ToString();
+            rangeCard.SetActive(true);
+            StatDisplays.Add(rangeCard);
 
-        var moveCard = Instantiate(StatCard, StatCard.transform.parent);
-        moveCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.Movement);
-        moveCard.transform.GetChild(1).GetComponent<Text>().text = unit.Movement.ToString();
-        moveCard.SetActive(true);
-        StatDisplays.Add(moveCard);
+            var moveCard = Instantiate(StatCard, StatCard.transform.parent);
+            moveCard.transform.GetChild(0).GetComponent<Image>().sprite = GetSprite(unit.Movement);
+            moveCard.transform.GetChild(1).GetComponent<Text>().text = unit.Movement.ToString();
+            moveCard.SetActive(true);
+            StatDisplays.Add(moveCard);
 
-        StatPanel.SetActive(true);
+            StatPanel.SetActive(true);
+        }
     }
 
     public void HideMergePreview()
